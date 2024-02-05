0b159af456a5d203817f0f07658c3c8a5b122254
Shivanand Roy <snrcodes@gmail.com>
2024-02-06 01:44:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ st.title("Ben - Form 10K  x  Benchmarking\n\n**powered by GPT-4 Turbo**")
 st.markdown(
     """
     <style>
-    @import url('https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,500;0,600;0,700;0,800;0,900;1,500;1,600;1,700;1,800;1,900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
     h1, h2, h3, h4, h5, h6, [class^="st-"] {
-			font-family: 'Exo 2', sans-serif !important;
+			font-family: 'Epilogue', sans-serif !important;
 			}
     </style>
     """,
